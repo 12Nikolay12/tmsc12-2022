e870c113682afd415a9276fe3770a8ccfaec5e08
12Nikolay12 <NikolayDobrovolskiy1999@gmail.com>
2022-01-26 14:00:33 +0300

    2 Домашка

---
+++ Lesson4/src/main/java/HomeWork1.java
@@ -0,0 +1,34 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+        //1) Задача на оператор switch!
+//        Рандомно генерируется число От 1 до 7.
+//        Если число равно 1, выводим на консоль “Понедельник”, 2 –”Вторник” и так далее.
+//        Если 6 или 7 – “Выходной”.
+        double day = Math.random() * 7;
+        switch ((int)day) {
+            case 1: //month == 1
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Not a Day");
+        }
+    }
+}
