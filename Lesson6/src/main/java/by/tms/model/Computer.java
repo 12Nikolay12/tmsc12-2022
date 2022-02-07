4a16d274ee71929e430f7ee62b8bc6b6802d7777
12Nikolay12 <NikolayDobrovolskiy1999@gmail.com>
2022-02-07 17:01:51 +0300

    6 Домашка

---
+++ Lesson6/src/main/java/by/tms/model/Computer.java
@@ -0,0 +1,60 @@
+package by.tms.model;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Computer {
+    private String cpu;
+    private String ram;
+    private String hdd;
+    private int cycleResource;
+    private boolean burn;
+
+    public Computer(String cpu, String ram, String hdd, int cycleResource) {
+        this.cpu = cpu;
+        this.ram = ram;
+        this.hdd = hdd;
+        this.cycleResource = cycleResource;
+    }
+
+    public void on() {
+        do {
+            if (cycleResource > 0) {
+                System.out.println("Внимание! Введите 0 или 1");
+                Scanner scanner = new Scanner(System.in);
+                int personValue = scanner.nextInt();
+                Random random = new Random();
+                int randomValue = random.nextInt(2);
+                if (personValue == randomValue) {
+                    off();
+                } else {
+                    burn();
+                }
+                System.out.println("Осталось циклов " + cycleResource--);
+            } else {
+                burn();
+            }
+        } while (cycleResource > 0);
+    }
+
+    public void off() {
+        System.out.println("Выключение компьютера");
+        cycleResource--;
+    }
+
+    public void burn() {
+        System.out.println("Компьютер сгорел");
+        cycleResource = 0;
+        burn = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "cpu='" + cpu + '\'' +
+                ", ram='" + ram + '\'' +
+                ", hdd='" + hdd + '\'' +
+                ", cycleResource=" + cycleResource +
+                '}';
+    }
+}
